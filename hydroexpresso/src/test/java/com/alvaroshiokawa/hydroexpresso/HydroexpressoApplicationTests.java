c752a36ea8857d274773f9d296066adeccf676c2
AlvaroShiokawa <alvaro.shiokawa@gmail.com>
2022-02-09 13:25:37 -0300

    Initial commit

---
+++ hydroexpresso/src/test/java/com/alvaroshiokawa/hydroexpresso/HydroexpressoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvaroshiokawa.hydroexpresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydroexpressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
